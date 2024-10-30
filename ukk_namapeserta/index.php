8a50fcb8a5ea987bce81ac8e312cbd4ce00275b5
KUMBANG PRISTEL <144641207+Samsul1234567@users.noreply.github.com>
2024-10-30 13:14:04 +0700

    ORAAA

---
+++ ukk_namapeserta/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Website Galeri Foto</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    </head>   
+<body>
+    
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Website galeri foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+       
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+    
+<div class="container mt-3">
+    <div class="row">
+    <div class="col-md-3">
+
+    </div>
+   <script type="text/javascript" src="assets/js/bootstrap.min.js"></script> 
+</body>
+</html>
